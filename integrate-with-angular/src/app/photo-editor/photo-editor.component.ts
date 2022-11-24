d41bf4deedcfdc1c10df37df4df63d2bc6eb688c
Alan Uecker <alan.uecker@img.ly>
2022-11-24 09:10:43 +0100

    v5.17.3

--- integrate-with-angular/src/app/photo-editor/photo-editor.component.ts
+++ integrate-with-angular/src/app/photo-editor/photo-editor.component.ts
@@ -37,7 +37,7 @@ export class PhotoEditorComponent implements AfterViewInit {
         container: this.container ? this.container.nativeElement : '',
         image: this.src,
         assetBaseUrl:
-          'https://cdn.img.ly/packages/imgly/photoeditorsdk/5.17.2/assets',
+          'https://cdn.img.ly/packages/imgly/photoeditorsdk/5.17.3/assets',
       });
     } catch (error) {
       console.log(error);
